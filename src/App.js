47e8f182d371fdd11024d1638ed3778bc7320be2
Rishi Ranjan <101rishidsr@gmail.com>
2025-04-11 06:46:04 +0530

    testing...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import styled, { ThemeProvider } from "styled-components";
 import { darkTheme } from "./utils/Themes";
 import Navbar from "./components/Navbar";
-import { BrowserRouter } from "react-router-dom";
+
 import Hero from "./components/sections/Hero";
 import Skills from "./components/sections/Skills";
 import Experience from "./components/sections/Experience";
